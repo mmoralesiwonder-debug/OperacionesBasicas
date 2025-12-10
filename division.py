3e93eed7d07779a03baf85f5869bf7c504c7df4a
mmoralesiwonder-debug <mmoralesiwonder@gmail.com>
2025-12-10 08:27:06 -0600

    Update division.py

--- division.py
+++ division.py
@@ -1,7 +1,1 @@
-n1 = float(input("ingresa el primer numero:"))
-n2 = float(input("ingresa el segundo numero:"))
-n3 = float(input("ingresa el tercer numero:"))
 
-resultado = n1 / n2 / n3 
-
-print("el resultado de tu division es:", resultado)
