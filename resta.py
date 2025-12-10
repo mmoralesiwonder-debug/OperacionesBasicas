f5204af43882b907dc8a6fdad98c825ccbb0a578
mmoralesiwonder-debug <mmoralesiwonder@gmail.com>
2025-12-10 08:12:30 -0600

    Add files via upload

---
+++ resta.py
@@ -0,0 +1,7 @@
+n1 = float(input("ingresa el primer numero:"))
+n2 = float(input("ingresa el segundo numero:"))
+n3 = float(input("ingresa el tercer numero:"))
+
+resultado = n1 - n2 - n3 
+
+print("el resultado de tu resta es:", resultado)
