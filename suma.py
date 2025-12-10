0a9df6707e74f028f244a14391d0d45b8448f3e0
mmoralesiwonder-debug <mmoralesiwonder@gmail.com>
2025-12-10 08:33:56 -0600

    Update suma.py

--- suma.py
+++ suma.py
@@ -1,7 +1,15 @@
+print("calculadora de 3 numeros")
+print("1. sumar")
+print("2. restar")
+print("3. multiplicar")
+print("4. dividir")
+
+opcion = int(input("elige una opcion (1-4):"))
+
 n1 = float(input("ingresa el primer numero:"))
 n2 = float(input("ingresa el segundo numero:"))
 n3 = float(input("ingresa el tercer numero:"))
 
-resultado = n1 + n2 + n3 
-
-print("el resultado de tu suma es:", resultado)
+if opcion == 1:
+  resultado= n1 + n2 + n3
+  print("la suma es:", resultado)
